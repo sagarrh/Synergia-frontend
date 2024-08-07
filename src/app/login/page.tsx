d60d24a0796108babf1607ea7d267fdbca80c46f
Yash <yashmadhani2810@gmail.com>
2024-08-07 19:55:16 +0530

    Added signup form

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+function login() {
+  return (
+    <div>login</div>
+  )
+}
+
+export default login
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+import { SignUpComponent } from '@/components/component/SignUpComponent';
+
+function Signup() {
+  return (
+    <div className='p-36'>
+        <SignUpComponent/>
+    </div>
+  )
+}
+
+export default Signup
