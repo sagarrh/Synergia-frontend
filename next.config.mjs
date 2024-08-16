a427f243dae1fe91d980618bdb6e3e7ff5574cf8
Yash <yashmadhani2810@gmail.com>
2024-08-16 21:42:50 +0530

    Removed eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['assets.aceternity.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['assets.aceternity.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
